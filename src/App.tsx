28ab9395b959e9f91e77c21c332ce1df25a43a1d
nakul-hyper <nakul@hyperready.tech>
2025-11-10 13:10:51 +0530

    feat: add Card1 component and integrate it into App

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Card1 } from "./cards";
+
+export default function App() {
+  return (
+    <Card1 />
+  );
+}
---
+++ src/cards.tsx
@@ -0,0 +1,7 @@
+export function Card1(){
+  return (
+    <div>
+      <h1>Sample</h1>
+    </div>
+  )
+}
